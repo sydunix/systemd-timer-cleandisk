04209286d58a8c88d89480a5a94e99f9e2b5a3a6
Sydney Ekpo <sydney.j.ekpo@gmail.com>
2023-01-23 22:32:36 +0100

    Create uninstall-cleandisk.sh

---
+++ uninstall-cleandisk.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo systemctl stop cleanDisk.timer
+sudo systemctl disable cleanDisk.timer
+sudo systemctl daemon-reload
+sudo systemctl status cleanDisk.timer
+sudo rm /opt/cleanDisk.sh /etc/systemd/system/cleanDisk.service /etc/systemd/system/cleanDisk.timer
+sudo systemctl status cleanDisk.timer
+
+echo -e "SETUP\v SUCCESSFULLY\v COMPLETED!!"
